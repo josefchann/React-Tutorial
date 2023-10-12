7f7450537b404793f5fa2fdf50c29d6f3475c2b0
josefchann <hfyjc17@nottingham.edu.my>
2023-10-12 16:26:54 +0800

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./Components/Home"
+import Navbar from "./Components/Navbar"
+
+export default function App() {
+    return (  
+        <div>
+            <Home />
+            <Navbar />
+        </div>
+    )
+}
